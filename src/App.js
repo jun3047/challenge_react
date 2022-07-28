be7c0940ce1530d8621f476b0760ffe93f7e6652
jeong jun <jungjunjenon3047@gmail.com>
2022-07-29 05:37:41 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './conponent/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
   );
 }
---
+++ src/conponent/Test.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export default function Test({name}) {
+
+  return (
+    <p>{name}</p>
+  );
+}
