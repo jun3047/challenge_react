19ba36d4c061c13521bbf55ab604b49bdfb46295
jeong jun <jungjunjenon3047@gmail.com>
2022-07-30 02:35:36 +0900

    end

--- src/conponent/DataList.js
+++ src/conponent/DataList.js
@@ -1,15 +1,26 @@
-import { Link } from 'react-router-dom';
-import dummy from '../db/data.json'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function DataList() {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setDays(data);
+      });
+  }, []);
 
   return (
-    <ul className='list_day'>
-      {dummy.days.map(day => (
+    <ul className="list_day">
+      {days.map(day => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
       ))}
     </ul>
-  ); 
+  );
 }
